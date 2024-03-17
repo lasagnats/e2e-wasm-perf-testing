dc2b5b9d4a60d919ed491db0fae796d76376240b
lasagnats <lasagna.tss@gmail.com>
2024-03-17 18:36:18 +0200

    Add Performance Testing

--- BlazorClient/README.md
+++ BlazorClient/README.md
@@ -7,4 +7,4 @@ It is used for performance testing C#/Blazor client-side + C#/Blazor server-side
 ## Running the program
 
 Build the program via Build -> Publish in Visual Studio 2022
-Then serve it, for example, via `npx http-server -p 8087` from `<projectRoot>/blazor-impl/blazor-impl/bin/Release/net8.0/browser-wasm/publish/wwwroot`
+Then serve it, for example, via `npx http-server -p 8087` from `<projectRoot>/BlazoeClient/blazor-impl/bin/Release/net8.0/browser-wasm/publish/wwwroot`
---
+++ puppeteer/test_js_impl.js
@@ -0,0 +1,82 @@
+const puppeteer = require("puppeteer");
+
+const execCount = 60;
+
+
+//  Performance measuring script
+
+(async () => {
+  await runTestSuite(1000);
+  await runTestSuite(10000);
+})();
+
+async function runTestSuite(dataSize) {
+  const browser = await puppeteer.launch({ headless: false,      args: [
+    '--disable-web-security',
+    '--disable-features=IsolateOrigins',
+    '--disable-site-isolation-trials'
+]});
+  const page = await browser.newPage();
+
+  let timeStorage = [];
+
+
+  await page.goto("http://127.0.0.1:8087/");
+  await page.waitForSelector(`#entry-count`, { visible: true });
+
+  await page.$eval(`#entry-count`, (input) => (input.value = ""));
+  await page.type(
+    `#entry-count`,
+    dataSize+""
+  );
+
+  for (let i = 0; i < execCount; i++) {
+      let res = await runTest(page, dataSize);
+      console.log('Finished running the test')
+      timeStorage[i] = { ...res, ...timeStorage[i] };
+  }
+
+  console.log(`Results:`);
+  console.log(JSON.stringify(timeStorage));
+
+
+  await browser.close();
+  // TODO: write results to an output file
+}
+
+
+
+async function runTest(page, dataSize) {
+  const initialHeapSize = (await page.metrics()).JSHeapUsedSize;
+
+  await page.click(`#clear`);
+
+  const startTime = performance.now();
+
+
+  await page.click(`#generate-data`);
+
+  // await page.waitForSelector(`#status-message`, { visible: true });
+
+  await page.evaluate((dataSize) =>
+     document.querySelector('ul').childNodes.length === dataSize
+  );
+
+  const endTime = performance.now();
+
+  const time = endTime - startTime;
+
+  const performanceMetrics = await page.metrics();
+  const heapSize = performanceMetrics.JSHeapUsedSize;
+  // In order to convert to megabytes
+  // const heapMB = performanceMetrics.JSHeapUsedSize / (1024 * 1024);
+  const deltaHeapSize = performanceMetrics.JSHeapUsedSize - initialHeapSize;
+  const testRunResult = {
+    heapSize,
+    deltaHeapSize,
+    time,
+  };
+
+
+  return testRunResult;
+}
