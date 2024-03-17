e00a6956b12f5d5c2eb40771e89bffee41aacbfb
Lasagna <lasagna.tss@gmail.com>
2024-03-17 21:04:53 +0200

    Finish Kotlin Client implementation

--- KotlinClient/src/wasmJsMain/kotlin/App.kt
+++ KotlinClient/src/wasmJsMain/kotlin/App.kt
@@ -1,18 +1,17 @@
-
 import kotlinx.browser.document
 import kotlinx.browser.window
 import kotlinx.serialization.json.Json
+import org.w3c.dom.Element
 import org.w3c.dom.HTMLInputElement
 
 
 class App(private var rowId: Int = 1) {
 
 
-
     fun setupListeners() {
         val generateBtn = document.getElementById("generate-data");
         val clearBtn = document.getElementById("clear");
-        val entryCount:HTMLInputElement = document.getElementById("entry-count") as HTMLInputElement;
+        val entryCount: HTMLInputElement = document.getElementById("entry-count") as HTMLInputElement;
         val dataItems = document.getElementById("data-items");
 
 
@@ -22,74 +21,28 @@ class App(private var rowId: Int = 1) {
                 if (it.ok) {
                     it.text().then {
                         try {
-                            val obj = Json.decodeFromString<List<Entry>>(it.toString())
-                            println(obj.size) // MyModel(a=42, b="42")
+                            val entries = Json.decodeFromString<List<Entry>>(it.toString())
+                            for (entry in entries) {
+                                dataItems?.appendChild(getDataItem(entry))
+                            }
                         } catch (exception: Exception) {
                             println("${exception.message}")
                         }
-
-//                        for (row in rows) {
-//                                table?.appendChild(getTableRow(row))
-//                            }
-//                        println(it)
                         null
                     }
-
                 }
                 null
             }
-//            val rows: List<Entry> = buildData();
-//            for (row in rows) {
-//                table?.appendChild(getTableRow(row))
-//            }
         }
-
-//        addBtn?.addEventListener("click") {
-//            val rows: List<Entry> = buildData();
-//            for (row in rows) {
-//                table?.appendChild(getTableRow(row))
-//            }
-//        }
-//
-//        runLotsBtn?.addEventListener("click") {
-//            table?.textContent = ""
-//            val rows: List<Entry> = buildData(10000);
-//            for (row in rows) {
-//                table?.appendChild(getTableRow(row))
-//            }
-//        }
 
         clearBtn?.addEventListener("click") {
             dataItems?.textContent = ""
         }
-
-
-
     }
 
-//    private fun getTableRow(entry: Entry): Element {
-//        val tr = document.createElement("tr");
-//        val rowId = "entry" + entry.id.toString();
-//        tr.id = rowId;
-//        tr.innerHTML =
-//            "<td class='col-md-1'></td><td class='col-md-4'><a class='lbl'></a></td><td class='col-md-1'><a class='remove'><span class='remove glyphicon glyphicon-remove' aria-hidden='true'></span></a></td><td class='col-md-6'></td>";
-//        tr.getElementsByClassName("remove").get(0)?.addEventListener("click") {
-//            delete(entry.id)
-//        }
-//        tr.addEventListener("click") {
-//
-//            unselect();
-//            selectedRowID = rowId;
-//            tr.classList.add("danger")
-//        }
-//        val td1 = tr.firstChild;
-//        val a2 = td1?.nextSibling?.firstChild;
-//
-//        td1?.textContent = entry.id.toString()
-//        a2?.textContent = entry.label
-//
-//        return tr
-//    }
-
-
+    private fun getDataItem(entry: Entry): Element {
+        val li = document.createElement("li");
+        li.innerHTML = "<span>${entry.id}</span> <span>${entry.label}</span>"
+        return li;
+    }
 }
