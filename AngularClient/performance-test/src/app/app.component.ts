9c26a0e7fcde2db63f8dbeebb47899fb4d4d1e10
Lasagna <lasagna.tss@gmail.com>
2024-03-21 15:40:37 +0200

    Add JavaScript implementation

---
+++ AngularClient/performance-test/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Entry } from './models/entry';
+import { HttpClient } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'performance-test';
+  entryCount = 0;
+  entries: Entry[] = [];
+
+  constructor(private http: HttpClient){}
+
+  generateData() {
+      const url = `http://localhost:3000/data/${this.entryCount}`;
+
+      this.http.get(url)
+        .pipe(
+          catchError(error => {
+            // Handle error if needed
+            return throwError(error);
+          })
+        )
+        .subscribe(response => {
+          // Handle response if needed
+          this.entries = response as Entry[];
+        });
+  }
+
+  clear() {
+    this.entries = [];
+  }
+}
